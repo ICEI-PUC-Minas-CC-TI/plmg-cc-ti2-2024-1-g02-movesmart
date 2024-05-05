c6f99c4880ace3a3425c2a5604ddfc05938a826e
Yan Sabarense <sabarensey@gmail.com>
2024-05-05 10:28:36 -0300

    [UPDATE] Exclusão de arquivo não utilizado

--- Codigo/FrontEnd/js/notificacao.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('email-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var email = document.getElementById('email-input').value;
-    if (email.trim() !== '') {
-        alert('Obrigado por se inscrever para receber notificações por email!');
-    }
-});
